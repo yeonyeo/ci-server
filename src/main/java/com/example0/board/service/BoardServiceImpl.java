ec3f8418434c6240588e167431dace29363c39f9
lsy <sylim0513@gmail.com>
2024-05-21 10:30:23 +0900

    2

--- src/main/java/com/example0/board/service/BoardServiceImpl.java
+++ src/main/java/com/example0/board/service/BoardServiceImpl.java
@@ -18,6 +18,7 @@ public class BoardServiceImpl implements BoardService {
     public void insertBoard(BoardRequest boardRequest) {
         boardRepository.save(boardRequest.toEntity());
     }
+    //
 
     @Override
     public List<Board> getBoards() {
