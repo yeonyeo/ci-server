9e13ebf82b04ba77d0a970c94e42659682c26fb1
lsy <sylim0513@gmail.com>
2024-06-02 20:53:16 +0900

    [feature]add comments

--- 0520_FE/0520_fe/Dockerfile
+++ 0520_FE/0520_fe/Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx
 COPY build /react
-COPY default.cof /etc/nginx/conf.d/default.conf
+COPY default.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
--- 0520_FE/0520_fe/src/App.jsx
+++ 0520_FE/0520_fe/src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
-  const [name, setName] = useState(0);
-  const [text, setText] = useState(0);
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
   const [data, setData] = useState();
+  const [comment, setComment] = useState("");
+
   const getData = async () => {
     try {
       const response = await axios.get("/api/boards");
@@ -28,23 +30,53 @@ function App() {
       console.log(error);
     }
   };
+  const getComments = async () => {
+    try {
+      const response = await axios.get("/api/comments");
+      setData(response.data);
+      console.log(response);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const postComments = async () => {
+    try {
+      const response = await axios.get("/api/comments", {
+        comment: comment,
+      });
+      console.log(response);
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
 
-  useEffect(() => {
-    getData();
-  }, []);
+useEffect(() => {
+  getData();
+  getComments(); // /api/comments 엔드포인트 호출
+}, []);
 
-  return (
+return (
+  <div>
+    <input type="text" onChange={(e) => setName(e.target.value)}></input>
+    <input type="text" onChange={(e) => setText(e.target.value)}></input>
+    <button onClick={postData}>submit</button>
+    {data?.map((element) => (
+      <div>
+        name:{element.name} title:{element.text}
+      </div>
+    ))}
     <div>
-      <input type="text" onChange={(e) => setName(e.target.value)}></input>
-      <input type="text" onChange={(e) => setText(e.target.value)}></input>
-      <button onClick={postData}>submit</button>
-      {data?.map((element) => (
-        <div>
-          name:{element.name} title:{element.text}
-        </div>
+      <input type="a" onChange={(e) => setComment(e.target.value)}></input>
+      <button>Comment</button>
+      <button onClick={postComments}>open</button>
+      {comment.map((items) => (
+        <div key={items.id}>{items.text}</div>
       ))}
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
